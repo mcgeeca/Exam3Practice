933b631c854dd33f572ec4ff6c509aafa0a8cd90
mcgeeca <mcgeeca@pg.com>
2018-02-19 14:57:06 -0500

    DONE

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS in PRINTING-TO-CONSOLE problems.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Craig McGee Jr.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -75,7 +75,7 @@ def shape(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     ####################################################################
@@ -87,7 +87,17 @@ def shape(r):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  15 minutes.
     # ------------------------------------------------------------------
-
+    for k in range(r):
+        for s in range(k):
+            print(" ", end="")
+        for p in range(r-k):
+            print("+", end="")
+        print("!", end="")
+        for n in range(r-k):
+            print(r-n-k, end="")
+        for d in range(k):
+            print("-", end="")
+        print()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
